af4d8379a3a65a027c388fac8fd226934465b91c
Betide Studio <codingwithdevesh@gmail.com>
2024-07-12 15:02:44 +0530

    Added Authentication Docs and cleaned up along with adding icon

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
 
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-});
-
-module.exports = withNextra(nextConfig);
+module.exports = withNextra()
